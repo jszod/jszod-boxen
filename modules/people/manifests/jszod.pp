3ad215f73a39b66724c3940a82d8d1e6a5036df7
jszod <joe.szod@gmail.com>
2014-07-11 21:36:18 -0400

    Added MacVim to applications

--- modules/people/manifests/jszod.pp
+++ modules/people/manifests/jszod.pp
@@ -1,19 +1,38 @@
+# Customizations for jszod
 class people::jszod {
-   notify { 'class people::jszod declared': }
+  notify { 'class people::jszod declared': }
 
-   include appcleaner
-<<<<<<< HEAD
-   include hipchat
-   include skype
-   include omnigraffle
+  # Setup .dotfiles variables
+  $home = "/Users/${::boxen_user}"
+  $dotfiles_dir = "${boxen::config::srcdir}/dotfiles"
 
-   # Specify the global ruby version
-   # Somehow setting the ruby::global::version varialbe in hiera does not work
-   # Set the value explicitly in a file to mimic the 'rbenv global' command
-   file { 'version':
-     path => '/opt/boxen/rbenv/version',
-     content => '2.1.1',
-   }
-=======
->>>>>>> 2effe5557bd4f539e5a703ad87d513736a23713b
+  # Apps
+  # ----------------------------------------------
+  include appcleaner
+  include hipchat
+  include skype
+##  include omnigraffle
+  include macvim
+
+  # Specify the global ruby version
+  # ----------------------------------------------
+  # Somehow setting the ruby::global::version varialbe in hiera does not work
+  # Set the value explicitly in a file to mimic the 'rbenv global' command
+  file { 'version':
+    path    => '/opt/boxen/rbenv/version',
+    content => '2.1.1',
+  }
+
+  # .dotfile configuration
+  # ----------------------------------------------
+  repository { $dotfiles_dir:
+    source => "${::github_login}/dotfiles"
+  }
+
+  file { "${home}/.vimrc":
+    ensure  => link,
+    target  => "${dotfiles_dir}/.vimrc",
+    require => Repository[$dotfiles_dir]
+  }
+
 }

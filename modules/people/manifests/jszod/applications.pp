91d31a11fdd2d4154dc4cd8a14c3e158325637bf
jszod <joe.szod@gmail.com>
2014-09-09 17:16:41 -0400

    Added omnigraffle, seperated out applications into applications.pp

---
+++ modules/people/manifests/jszod/applications.pp
@@ -0,0 +1,18 @@
+#
+# Applications specific to Joe Szod (jszod)
+#
+class people::jszod::applications {
+  
+  include 'appcleaner'
+  include 'hipchat'
+  include 'skype'
+  include 'omnigraffle'
+  include 'macvim'
+  include 'tmux'
+  include 'java'
+  include 'xmind'
+  include 'sourcetree'
+  include 'iterm2::stable'
+  include 'syncplicity'
+
+} 
---
+++ modules/people/manifests/jszod/sublime_text.pp
@@ -0,0 +1,19 @@
+# 
+# Sublime Text settings specific for Joe Szod (jszod)
+#
+class people::jszod::sublime_text {
+
+  include 'sublime_text'
+
+  $home = "/Users/${::boxen_user}"
+
+  file { "${home}/Library/Application Support/Sublime Text 3/Packages/User":
+    ensure  => 'directory',
+    owner   => $::boxen_user,
+    mode    => '0755'
+  }
+
+
+}
+
+
